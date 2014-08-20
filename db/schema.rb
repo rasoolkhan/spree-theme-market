929cb0848354e5bef3d9dc287d4bec7acdd8bc68
tracymu <tracy.musung@moomumedia.com>
2014-08-20 18:27:34 +1000

    Ran migration for administrator model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140817055859) do
+ActiveRecord::Schema.define(version: 20140820082409) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "administrators", force: true do |t|
+    t.string   "email"
+    t.string   "username"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "authors", force: true do |t|
     t.string   "name"
