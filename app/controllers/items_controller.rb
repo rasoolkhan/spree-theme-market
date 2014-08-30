d661d3ef826511ac151ed0290b60a1c4af728ae0
tracymu <tracy.musung@moomumedia.com>
2014-08-31 08:55:31 +1000

    Move fake data into controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
 
   def index
-  end
+    fake_item_1 = Item.new(name: "Spring theme", description: "Spring colors")
+    fake_item_2 = Item.new(name: "Summer theme", description: "Spring colors")
+    fake_item_3 = Item.new(name: "Winter theme", description: "Spring colors")
 
+    @items = [fake_item_1, fake_item_2, fake_item_3]
+  end
 end
