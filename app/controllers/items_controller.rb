fdf95746e787b1c8e4df6349a1fa32b585255a9b
tracymu <tracy.musung@moomumedia.com>
2014-09-01 19:25:00 +1000

    Update to use pure css grid

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,8 @@
 class ItemsController < ApplicationController
-
   def index
     fake_item_1 = Item.new(name: "Spring theme", description: "Spring colors")
     fake_item_2 = Item.new(name: "Summer theme", description: "Spring colors")
     fake_item_3 = Item.new(name: "Winter theme", description: "Spring colors")
-
     @items = [fake_item_1, fake_item_2, fake_item_3]
   end
 end
