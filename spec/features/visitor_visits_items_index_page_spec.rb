cef7137ec0e3dc56ffce89dbac5d0fbeb574e033
tracymu <tracy.musung@moomumedia.com>
2014-09-04 20:08:07 +1000

    Add have_css test

--- spec/features/visitor_visits_items_index_page_spec.rb
+++ spec/features/visitor_visits_items_index_page_spec.rb
@@ -2,15 +2,14 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe "the items index page" do
-  it "shows the Spring theme" do
+  before :each do
     visit '/items'
+  end  
+  it "shows the Spring theme" do
     expect(page).to have_content "Spring" 
   end
 
   it "shows 9 product divs" do
-    expect(page).to have_css 'div#pure-u-1-3', :count => 9
+    expect(page).to have_css('div .box'), :count => 9
   end
-
-  # expect to see x products rendered
-  # check css is there
 end
