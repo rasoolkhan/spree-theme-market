ed6e27c27b137bcb9413035e9dbabe7ab79a33ad
tracymu <tracy.musung@moomumedia.com>
2014-09-04 19:45:28 +1000

    Capybara test

---
+++ spec/features/visitor_visits_items_index_page_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'spec_helper'
+
+describe "the items index page" do
+  it "shows the Spring theme" do
+    visit '/items'
+    expect(page).to have_content "Spring" 
+  end
+  
+  it "shows 9 product divs" do
+    expect(page).to have_css 'div#pure-u-1-3', :count => 9
+  end
+  
+  # expect to see x products rendered
+  # check css is there
+end
