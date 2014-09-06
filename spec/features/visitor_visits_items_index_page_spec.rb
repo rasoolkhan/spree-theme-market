f7a618a04df4b19ffd6ac13200df66935865d00b
tracymu <tracy.musung@moomumedia.com>
2014-09-06 10:10:25 +1000

    Update test to better format

--- spec/features/visitor_visits_items_index_page_spec.rb
+++ spec/features/visitor_visits_items_index_page_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe "the items index page" do
-  before :each do
+  before do
     visit '/items'
   end  
   it "shows the Spring theme" do
@@ -10,10 +10,10 @@ describe "the items index page" do
   end
 
   it "shows 9 product divs" do
-    expect(page).to have_css('div .box'), :count => 9
+    expect(page).to have_css('div .box'), count: 9
   end
   
   it "shows 9 images" do
-    expect(page).to have_css('.box img'), :count => 9
+    expect(page).to have_css('.box img'), count: 9
   end
 end
