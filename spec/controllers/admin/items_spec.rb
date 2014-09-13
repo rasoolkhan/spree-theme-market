231fd9e0f5b96f3987af7e87bd46e0f0a2b0ab13
tracymu <tracy.musung@moomumedia.com>
2014-09-13 17:13:14 +1000

    Create Admin controller spec - not finished

---
+++ spec/controllers/admin/items_spec.rb
@@ -0,0 +1,59 @@
+require "spec_helper"
+require "rails_helper"
+
+describe Admin::ItemsController do
+  describe "#index" do
+    before do
+      # Below I am trying to make something to overcome our http authentication
+      # It doesn't work, so in my files I comment out the authentication line in 
+      # admin_controller.rb
+      
+      # allow_any_instance_of(AdminController).to receive(:http_basic_authenticate_with) { true }
+      @item = FactoryGirl.create(:item)
+      get :index
+    end
+       
+    it "responds with status 200" do
+      expect(response).to have_http_status("200")
+    end
+    
+    it "renders items/index template" do
+      expect(response).to render_template("index") or admin/items/index
+    end
+
+    it "loads all of the items" do
+      # This test does not pass and I can't figure out why.
+      expect(assigns(:items)).to eq([@item])
+    end
+  end
+  
+  describe "#new" do
+    before do
+      # allow_any_instance_of(AdminController).to receive(:http_basic_authenticate_with) { true }
+      get :new
+    end
+       
+    it "responds with status 200" do
+      expect(response).to have_http_status("200")
+    end
+    
+    it "renders items/new template" do
+      expect(response).to render_template("new") or admin/items/new
+    end
+  end
+    
+  describe "#create" do
+  end
+
+  describe "#show" do 
+  end
+  
+  describe "#edit" do
+
+  end
+  
+  describe "#delete" do
+
+  end
+end
+
