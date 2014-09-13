73640458cb060d04e8411995e98abab6cd931693
tracymu <tracy.musung@moomumedia.com>
2014-09-13 17:02:21 +1000

    Create first factory

---
+++ spec/factories/items.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :item do
+    name 'Croc Theme'
+    description 'Some lorem ipsum'
+    price 10
+    code '789789'
+    image_preview 'http://placebear.com/200/200'
+  end
+end
