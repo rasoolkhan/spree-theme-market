37cf11f9174645feab02e2f6435f9e790d6bc2c5
tracymu <tracy.musung@moomumedia.com>
2014-09-22 19:50:23 +1000

    Refactor migration to remove self.

--- db/migrate/20140915102648_add_attachment_attachment_to_items.rb
+++ db/migrate/20140915102648_add_attachment_attachment_to_items.rb
@@ -1,11 +1,11 @@
 class AddAttachmentAttachmentToItems < ActiveRecord::Migration
-  def self.up
+  def up
     change_table :items do |t|
       t.attachment :attachment
     end
   end
 
-  def self.down
+  def down
     remove_attachment :items, :attachment
   end
 end
