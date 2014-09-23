0dfdcfeaa19c4b80af5ef8c5634f86ed752fd6f9
tracymu <tracy.musung@moomumedia.com>
2014-09-23 19:20:21 +1000

    Fix sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.user_id
-      redirect_to admin_items_path, :notice => "Logged In!"
+    user = User.find_by_email(session_params[:email])
+    if user && user.authenticate(session_params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_items_path, notice: "Logged In!"
     else
       flash.now.alert = "Invalid email or password"
       render 'new'
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
   
   def destroy
     session[:user_id] = nil
-    redirect_to items_url, :notice => "Logged Out"
+    redirect_to items_url, notice: "Logged Out"
   end
   
   protected
