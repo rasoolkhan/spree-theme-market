9fb2f62935ead00916029e109744f920fddd5f4a
tracymu <tracy.musung@moomumedia.com>
2014-09-23 19:35:45 +1000

    Move authentication from admin controller to application controller

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,15 +1,3 @@
 class AdminController < ApplicationController 
-  # http_basic_authenticate_with name: "tracy", password: "super_secure"
   before_action :authenticate!
-  
-  def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  end
-  
-  helper_method :current_user
-  
-  private
-  def authenticate!
-    redirect_to login_url unless current_user
-  end
 end
